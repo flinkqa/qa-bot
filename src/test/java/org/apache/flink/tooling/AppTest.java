de0f342cc163eb014d8f3f9f503d4197b202d3fa
Maximilian Michels <mxm@apache.org>
2015-05-11 19:19:31 +0200

    add some simple tests for the command output

--- src/test/java/org/apache/flink/tooling/AppTest.java
+++ src/test/java/org/apache/flink/tooling/AppTest.java
@@ -4,35 +4,53 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.io.IOException;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+	extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AppTest( String testName )
+	{
+		super( testName );
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite()
+	{
+		return new TestSuite( AppTest.class );
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp()
+	{
+	}
+
+	public void testRunCommandStdout() {
+		String expected = "testing";
+		String output = App.runCommand("echo", "-n", expected);
+		assertEquals(expected, output);
+	}
+
+	public void testRunCommandStderr() {
+		String className = "ILLEGALCLASS";
+		String output = App.runCommand("java", className);
+		assertEquals("Error: Could not find or load main class " + className + "\n", output);
+	}
+
+	public void testIllegalCommand() {
+		String output =	App.runCommand("ILLEGALPROGRAM");
+		assertTrue(output.startsWith("Error running"));
+	}
 }
